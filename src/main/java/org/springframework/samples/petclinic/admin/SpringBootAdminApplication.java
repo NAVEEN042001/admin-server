e1358a9f786ac7b54932ac921397ffe55f58d7ff
NaveenSubramaniyan <124786174+NAVEEN042001@users.noreply.github.com>
2024-09-25 16:07:48 +0530

    Update SpringBootAdminApplication.java

--- src/main/java/org/springframework/samples/petclinic/admin/SpringBootAdminApplication.java
+++ src/main/java/org/springframework/samples/petclinic/admin/SpringBootAdminApplication.java
@@ -3,7 +3,7 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * you may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -16,16 +16,40 @@
 package org.springframework.samples.petclinic.admin;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import io.rollout.rox.server.Rox;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import io.rollout.rox.server.Rox;
+
+import java.util.concurrent.ExecutionException;
 
 @SpringBootApplication
 @EnableAdminServer
 @EnableDiscoveryClient
 public class SpringBootAdminApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootAdminApplication.class, args);
-    }
 
+
+    private static final Logger log = LoggerFactory.getLogger(SpringBootAdminApplication.class);
+
+    public static void main(String[] args) {
+        try {
+            // Initialize the Rox SDK with the feature management environment key
+            Rox.setup("6e3b0d18-4288-4d77-4843-b5a51a72e24b").get();
+
+            // Initialize and register Flags
+            Flags.register();
+
+            // Print the value of the boolean enableTutorial flag
+            log.info("enableTutorial value is %s%n", Flags.enableTutorial.isEnabled() ? "true" : "false");
+            System.out.printf("enableTutorial value is %s%n", Flags.enableTutorial.isEnabled() ? "true" : "false");
+
+            // Start the Spring Boot application
+            SpringApplication.run(SpringBootAdminApplication.class, args);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
 }
